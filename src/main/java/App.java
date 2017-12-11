7cbd518c550ca85151751dfe2a2cd5b33614c95d
peterkariukimutuura <kariukipeter30@gmail.com>
2017-12-11 19:23:38 +0300

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,59 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App{
+	public static void main(String[] args) {
+		staticFileLocation("/public");
+		String layout ="templates/layout.vtl";
+		System.out.println("we are ready to start");
+
+		    ProcessBuilder process = new ProcessBuilder();
+		     Integer port;
+		     if (process.environment().get("PORT") != null) {
+		         port = Integer.parseInt(process.environment().get("PORT"));
+		     } else {
+		         port = 4567;
+		     }
+		    setPort(port);
+
+		    //somethings to do with port issues
+
+		    get("/",(request,response)->{
+		    	Map<String,Object> model= new HashMap<String,Object>();
+		    	model.put("template","templates/insertnewhero.vtl");
+		    	return new ModelAndView(model,layout);
+		    }, new VelocityTemplateEngine());
+
+
+		    //handling post requests
+
+		    post("/newhero",(request,response)->{
+		    	Map <String,Object> model = new HashMap<String,Object>();
+		    	String name =request.queryParams("name");
+		    	int age = Integer.parseInt(request.queryParams("age"));
+		    	String specialpowers =request.queryParams("specialpowers");
+		    	String weakness =request.queryParams("weakness");
+
+		    	Hero newHero = new Hero(name,age,specialpowers,weakness);
+
+		    	model.put("template","templates/success.vtl");
+
+		    	return new ModelAndView(model,layout);
+		    },new VelocityTemplateEngine());
+
+		    //end  of handling post requests
+
+
+		    //retrieving all heroes
+
+		    get("/heros",(request,response)->{
+		    	Map <String,Object> model = new HashMap<String,Object>();
+		    	model.put("heros",Hero.all());
+		    	model.put("template","templates/heros.vtl");
+		    	return new ModelAndView(model,layout);
+		    },new VelocityTemplateEngine());
+	}
+}
---
+++ src/main/java/Hero.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public  class Hero{
+	private String newName;
+	private int newAge;
+	private int mId;
+	private String newSpecialpowers;
+	private String newWeakness;
+	private  static List<Hero> heros = new ArrayList<Hero>();
+
+	public Hero(String name,int age,String specialpowers,String weakness){
+		newName=name;
+		newAge=age;
+		newSpecialpowers=specialpowers;
+		newWeakness=weakness;
+		heros.add(this);
+		mId= heros.size();
+	}
+
+	public String getName(){
+		return newName;
+	}
+	public int getAge(){
+		return newAge;
+	}
+	public String getSpecialpowers(){
+		return newSpecialpowers;
+	}
+	public String getWeakness(){
+		return newWeakness;
+	}
+	public static List<Hero> all(){
+		return heros;
+	}
+	public static void clear(){
+		heros.clear();
+	}
+	public int getId(){
+		return mId;
+	}
+	public static Hero find(int id){
+		return heros.get(id-1);
+	}
+}
